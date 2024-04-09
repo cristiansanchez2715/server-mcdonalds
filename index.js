2ab0e9442d08b593d9c6dbfcf5e2f4b42b7273b4
cristiansanchez2715 <cristiansanchez2715@gmail.com>
2024-04-09 10:38:47 -0500

    Añadiendo date a la base de datos

--- index.js
+++ index.js
@@ -8,14 +8,7 @@ const http = require("http");
 const server = http.createServer(app);
 const mysql = require("mysql")
 
-// CONFIGURACIOENS GENERALES Y CONECCION WEBSOCKETS PARA LA REALIZACION DE PEDIDOS
 
-// app.use((req, res, next) => {
-//   res.header("Access-Control-Allow-Origin", "http://localhost:3000/admin");
-//   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-//   res.header("Access-Control-Allow-Headers", "Content-Type");
-//   next();
-// });
 app.use(express.json())
 app.options('*', (req, res) => {
   const allowedOrigins = ['https://cristiansanchez2715.github.io/admin', 'https://cristiansanchez2715.github.io'];
@@ -76,14 +69,14 @@ io.on("connection", (socket) => {
     // Aquí puedes procesar el pedido y enviarlo a la cocina, guardar en una base de datos, etc.
     // Por ejemplo, puedes emitir un evento para notificar a la cocina sobre el nuevo pedido.
     io.emit("pedidoALaCocina", data);
-    const { products, table, pay } = data;
+    const {products, table, pay, date} = data;
 
     // Convertir el objeto products a una cadena JSON
     const productsJSON = JSON.stringify(products);
    
     // Insertar datos en la tabla Pedidos
-    const sql = 'INSERT INTO pedidos ( table_number, products, totalPayOrder) VALUES (?, ?, ?)';
-    connection.query(sql, [table, productsJSON,  pay], (err, result) => {
+    const sql = 'INSERT INTO pedidos (table_number, products, totalPayOrder, date) VALUES (?, ?, ?, ?)';
+    connection.query(sql, [table, productsJSON,  pay, date], (err, result) => {
       if (err) {
         console.error('Error al insertar datos en la tabla Pedidos:', err);
         // Enviar mensaje de error al cliente a través del socket
@@ -97,40 +90,7 @@ io.on("connection", (socket) => {
   });
 });
 
-
-// io.on("connection", (socket) => {
-//   console.log("Cliente conectado");
-
-//   socket.on('nuevoPedido', (data) => {
-//     console.log("Nuevo pedido desde el cliente:", data);
-//     // Aquí puedes procesar el pedido y enviarlo a la cocina, guardar en una base de datos, etc.
-//     // Por ejemplo, puedes emitir un evento para notificar a la cocina sobre el nuevo pedido.
-//     io.emit("pedidoALaCocina", data);
-//     const { products, table, pay } = data;
-
-//     // Convertir el objeto products a una cadena JSON
-//     const productsJSON = JSON.stringify(products);
-   
-//     // Insertar datos en la tabla Pedidos
-//     const sql = 'INSERT INTO pedidos ( table_number, products, totalPayOrder) VALUES (?, ?, ?)';
-//     connection.query(sql, [table, productsJSON,  pay], (err, result) => {
-//       if (err) {
-//         console.error('Error al insertar datos en la tabla Pedidos:', err);
-//         res.status(500).json({ error: 'Error interno del servidor' });
-//         return;
-//       }
-//       console.log('Datos insertados en la tabla Pedidos');
-//       res.status(200).json({ message: 'Datos insertados correctamente en la tabla Pedidos' });
-//     });
-//   });
-// });
-
-
-
-
 // CONFIGURACIONES A LA BASE DE DATOS
-
-
 
 const connection = mysql.createConnection({
 'host': "be1zdultmwmtzebubor1-mysql.services.clever-cloud.com",
@@ -147,10 +107,6 @@ connection.connect((err) => {
     console.log("Conexión a la base de datos establecida");
   }
 });
-
-
-
-
 
 // enviar pedidos al admin
 
@@ -171,46 +127,4 @@ app.get('/dataBaseGet', (req, res) => {
 });
 
 // Configurar el servidor para escuchar en el puerto 4000
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// app.listen(port2, () => {
-//   console.log("server 2 conected")
-// })
-
-// app.post('/dataBaseSend', (req, res) => {
-//   console.log("esto esta llegando al backend" + JSON.stringify(req.body.data))
-//   const { products, table, pay } = req.body.data;
-
-//   // Convertir el objeto products a una cadena JSON
-//   const productsJSON = JSON.stringify(products);
- 
-//   // Insertar datos en la tabla Pedidos
-//   const sql = 'INSERT INTO pedidos (products, table_number, pay) VALUES (?, ?, ?)';
-//   connection.query(sql, [productsJSON, table, pay], (err, result) => {
-//     if (err) {
-//       console.error('Error al insertar datos en la tabla Pedidos:', err);
-//       res.status(500).json({ error: 'Error interno del servidor' });
-//       return;
-//     }
-//     console.log('Datos insertados en la tabla Pedidos');
-//     res.status(200).json({ message: 'Datos insertados correctamente en la tabla Pedidos' });
-//   });
-// });
 
